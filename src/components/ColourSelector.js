3c38c7ff3a50487d5718b97e371a491e8fe0126c
pratiksupee <135314162+pratiksupee@users.noreply.github.com>
2026-05-21 12:24:37 +0530

    done3

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -5,7 +5,9 @@ const ColourSelector = (props) => {
   const { background, classname, label } = config;
 
   return (
-    <button className={classname} onClick={() => changeColor(background)}>
+    <button className={classname}  
+    data-testid={config.key}
+    onClick={() => changeColor(background)}>
       {label}
     </button>
   )
