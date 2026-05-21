41c77c2bc5e0af5ac72b14a667b3f7d6e728ba10
pratiksupee <135314162+pratiksupee@users.noreply.github.com>
2026-05-21 12:14:02 +0530

    done2

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 
-const Selection = ({ applyColor }) => {
-  const [style, setStyle] = useState({ background: "" });
+const Selection = ({ currentColor }) => {
+  const [bgColor, setBgColor] = useState("");
 
   const handleClick = () => {
-    applyColor(setStyle);
+    setBgColor(currentColor);
   }
 
   return (
-    <div className="fix-box" style={style} onClick={handleClick}>
+    <div className="fix-box" style={{ background: bgColor }} onClick={handleClick}>
       <h3>Selection</h3>
     </div>
   )
